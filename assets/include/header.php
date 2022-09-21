c58b569a86e7b586aa1725b44ca64aafbc2b06d2
Mathis Lambert <mathis.lambert27@gmail.com>
2022-09-21 21:23:48 +0200

    init new branch and first commit

---
+++ assets/include/header.php
@@ -0,0 +1,14 @@
+<header class="header-mobile">
+    <div class="header-container">
+        <ul class="header-list">
+            <li class="header-item">
+                <a href="https://www.mathislambert.fr" class="header-link">
+                    <img src="./assets/img/logo/logo.png" alt="Logo" class="header-logo">
+                </a>
+            </li>
+            <li class="header-item dark-mode">
+
+            </li>
+        </ul>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+  <title>Portfolio | Mathis Lambert</title>
+
+  <!-- import css -->
+  <link rel="stylesheet" href="assets/style/style.css" />
+</head>
+
+<body>
+  <?php include 'assets/include/header.php'; ?>
+  <h1>Hello World !</h1>
+  <!-- Javascript import files -->
+  <script src="assets/app/app.js"></script>
+</body>
+
+</html>
