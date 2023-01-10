09de3405e4a22c4d923abd369e0996dba87735ff
Mathis Lambert <mathis.lambert27@gmail.com>
2023-01-10 18:57:16 +0100

    resolve conflict

--- assets/app/app.js
+++ assets/app/app.js
@@ -13,3 +13,38 @@ gsap.to(".panel", {
     markers: false,
   },
 });
+/*
+ * cursor part
+ */
+const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+const cursor_outer = document.querySelector(".cursor__outer");
+
+const cursorPos = {
+  x: 0,
+  y: 0,
+};
+
+const outerCursorPos = {
+  x: 0,
+  y: 0,
+};
+
+document.addEventListener("mousemove", (e) => {
+  cursorPos.x = e.clientX;
+  cursorPos.y = e.clientY;
+});
+
+const animate = () => {
+  outerCursorPos.x = lerp(outerCursorPos.x, cursorPos.x, 0.1);
+  outerCursorPos.y = lerp(outerCursorPos.y, cursorPos.y, 0.1);
+
+  const outerWidth = cursor_outer.getBoundingClientRect().width;
+
+  cursor_outer.style.transform = `translate3d(${outerCursorPos.x - 8}px, ${
+    outerCursorPos.y - 8
+  }px, 0)`;
+
+  requestAnimationFrame(animate);
+};
+animate();
